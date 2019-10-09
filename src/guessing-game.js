5fc441f2ccb8a84354a0d824922c4f3f02cfef4b
Ryntsevich Oksana <ryntsevich.oksana@gmail.com>
2019-10-09 19:21:31 +0300

    feat: implement task guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+
+    }
 
     setRange(min, max) {
+        this.minN = min;
+        this.maxN = max;
 
     }
 
     guess() {
+        return this.result = Math.ceil((this.minN + this.maxN) / 2);
 
     }
 
     lower() {
+        this.maxN = this.guess();
 
     }
 
     greater() {
+        this.minN = this.guess();
 
     }
 }
